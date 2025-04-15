594043b402472ee4bd0c85f6cb838d24ea9f9d34
Kacper Agatowski <n00212272@iadt.ie>
2025-04-15 02:39:26 +0100

    added extra contexts

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import { GlobalRefreshProvider } from "./contexts/GlobalRefreshContext";
+import { InboxCountProvider } from "./contexts/InboxCountContext";
 import App from "./App";
 import "./index.css";
 import { SidebarProvider } from "./components/ui/sidebar";
 
+// Properly nesting the context providers
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    
-    <AuthProvider>
-   
-      <BrowserRouter>
-      <SidebarProvider>
-        <App />
-      </SidebarProvider>
-      </BrowserRouter>
-     
-    </AuthProvider>
-    
+    <BrowserRouter>
+      <InboxCountProvider> 
+        <GlobalRefreshProvider>
+          <AuthProvider>
+            <SidebarProvider>
+              <App />
+            </SidebarProvider>
+          </AuthProvider>
+        </GlobalRefreshProvider>
+      </InboxCountProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
