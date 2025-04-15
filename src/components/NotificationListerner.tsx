c5b684ee0b20121224985e72e37303a1aa633de2
Kacper Agatowski <n00212272@iadt.ie>
2025-04-15 02:39:46 +0100

    WebSockets Added

---
+++ src/components/NotificationListerner.tsx
@@ -0,0 +1,52 @@
+import { useWebSocket } from "@/hooks/useWebSocket";
+import { useAuth } from "@/contexts/AuthContext";
+import { useState, useEffect } from "react";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert"; 
+import { useGlobalRefresh } from "@/contexts/GlobalRefreshContext";
+const NotificationListener = () => {
+  const { token, loading } = useAuth();
+  const [message, setMessage] = useState<string | null>(null);
+  const { setPageToRefresh } = useGlobalRefresh();
+
+  const handleMessage = (message: string) => {
+    console.log("🧠 WebSocket message received:", message);
+    setMessage(message); 
+    setPageToRefresh('notifications'); 
+  };
+
+  const { isConnected } = useWebSocket(!loading ? token : null, handleMessage);
+
+  
+  useEffect(() => {
+    if (isConnected) {
+      console.log("WebSocket connected");
+    } else {
+      console.log("WebSocket disconnected");
+    }
+  }, [isConnected]);
+
+  
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        setMessage(null); 
+      }, 10000); // 10 seconds
+
+      return () => clearTimeout(timer); 
+    }
+  }, [message]);
+
+  return (
+    <div>
+      {/* Render alert if there's a message */}
+      {message && (
+        <Alert className="bg-blue-500 text-white"> 
+          <AlertTitle>New Notification</AlertTitle>
+          <AlertDescription className="text-black">{message}</AlertDescription>
+        </Alert>
+      )}
+    </div>
+  );
+};
+
+export default NotificationListener;
---
+++ src/hooks/useWebSocket.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useWebSocket = (
+  token: string | null,
+  handleMessage: (message: string) => void
+) => {
+  const [isConnected, setIsConnected] = useState(false);
+  const wsRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    if (!token) {
+      console.warn("No token, skipping WebSocket connection.");
+      return;
+    }
+
+    const url = `ws://localhost:8000/ws?access_token=${token}`;
+    const ws = new WebSocket(url);
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      console.log("WebSocket connected");
+      setIsConnected(true);
+    };
+
+    ws.onmessage = (event) => {
+      console.log("Message received:", event.data);
+      handleMessage(event.data);
+    };
+
+    ws.onerror = (error: Event) => {
+      console.error("WebSocket error", error);
+    };
+
+    ws.onclose = (event: CloseEvent) => {
+      console.warn("WebSocket closed", {
+        code: event.code,
+        reason: event.reason,
+        wasClean: event.wasClean,
+      });
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [token]);
+
+  return { isConnected };
+};
