ac76ea1fcf6ed8bd766edf9a1ebdd12213229b86
Kacper Agatowski <n00212272@iadt.ie>
2025-05-04 01:47:33 +0100

    vercel

--- src/config/Api.tsx
+++ src/config/Api.tsx
@@ -1,8 +1,17 @@
+// import axios from 'axios';
+
+// const instance = axios.create({
+//     baseURL: 'http://127.0.0.1:8000',
+//     withCredentials: true
+// });
+
+// export default instance; 
+
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-    withCredentials: true
+    baseURL: import.meta.env.VITE_API_URL, 
+    withCredentials: true,
 });
 
-export default instance; 
+export default instance;
